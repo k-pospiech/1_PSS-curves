16237e46cc2a3be5142bbb8f2b372fcce6060afa
HatCherry <kpospiech.tg@gmail.com>
2023-08-26 21:07:06 +0200

    Updated PSS_data.py with creating and returning a compact case  name that can be used as a new file/sheet name

--- PSS_data.py
+++ PSS_data.py
@@ -1,34 +1,39 @@
 def PSS_data():
     """
     This function takes PSS output .xls files from the Input folder and exctracts data suitable to be used with curve fitting script
+    It returns string with compact name of the case, dictionary of temperature vs current, and dictionary of useful case data
 
     Example:
-    >>> a, b = PSS_data()
-    >>> print(a['ANSYS10mm2Copper_20230811_2305 - Copy.xls'], "\n", b['ANSYS10mm2Copper_20230811_2305 - Copy.xls'])
-                             0                   1
-    0    Nominal cross section                  10
-    1              Part number  ANSYS 10mm2 Copper
-    2      Insulation material                 PVC
-    3            Core diameter                3.57
-    4           Outer diameter                3.57
-    5           Filling factor               0.498
-    6      Ambient temperature                20.0
-    7        Operating current             155.000
-    8  Voltage drop per length              1.3272      
-          Current [A]  Temperature [degC]
-    0            0.0               20.00
-    1            0.2               20.00
-    2            0.3               20.00
-    3            0.5               20.01
-    4            0.6               20.01
-    ..           ...                 ...
-    995        154.4              403.17
-    996        154.5              403.90
-    997        154.7              404.64
-    998        154.8              405.38
-    999        155.0              406.11
+    >>> a, b, c = PSS_data()
+    >>> print("\n", a, "\n\n", b['ANSYS10mm2Copper_20230811_2305 - Copy.xls'], "\n\n", c['ANSYS10mm2Copper_20230811_2305 - Copy.xls'])
+ 
+10mm2_0.498fill_PVC_0.0_thick_20.0ambient 
 
-    [1000 rows x 2 columns]
+      Current [A]  Temperature [degC]
+0            0.0               20.00
+1            0.2               20.00
+2            0.3               20.00
+3            0.5               20.01
+4            0.6               20.01
+..           ...                 ...
+995        154.4              403.17
+996        154.5              403.90
+997        154.7              404.64
+998        154.8              405.38
+999        155.0              406.11
+
+[1000 rows x 2 columns]
+
+                          0                   1
+0    Nominal cross section                  10
+1              Part number  ANSYS 10mm2 Copper
+2      Insulation material                 PVC
+3            Core diameter                3.57
+4           Outer diameter                3.57
+5           Filling factor               0.498
+6      Ambient temperature                20.0
+7        Operating current             155.000
+8  Voltage drop per length              1.3272
     """
     import os
     import pandas as pd
@@ -69,6 +74,14 @@ def PSS_data():
         # print(Cable_info[key])
         # print("-" * 50)  # Optional line separator
 
+        # Compact name:
+        Cable_size = df[1][0]
+        Fill_factor = df[1][5]
+        Insulation_material = df[1][2]
+        Insulation_thickness = float(df[1][4])-float(df[1][3])
+        Ambient_temperature = df[1][6]
+        Name = "{}mm2_{}fill_{}_{}_thick_{}ambient".format(Cable_size,Fill_factor,Insulation_material,Insulation_thickness,Ambient_temperature)
+
     # Clean Current vs Temp data
     for key,df in Temp_v_Current.items():
         # print(key)
@@ -77,8 +90,8 @@ def PSS_data():
         df.iloc[:, 1] = df.iloc[:, 1].round(2)      # rounds the values in the Temperature column to two decimal places
         # print(Temp_v_Current[key])
         # print("-" * 50)     # Optional line separator
+        
+    return Name, Temp_v_Current, Cable_info
 
-    return Cable_info, Temp_v_Current
-
-a, b = PSS_data()
-print(a['ANSYS10mm2Copper_20230811_2305 - Copy.xls'], "\n", b['ANSYS10mm2Copper_20230811_2305 - Copy.xls'])
+a, b, c = PSS_data()
+print("\n", a, "\n\n", b['ANSYS10mm2Copper_20230811_2305 - Copy.xls'], "\n\n", c['ANSYS10mm2Copper_20230811_2305 - Copy.xls'])
