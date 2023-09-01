dd80dda64663212f0e91a06e6ac6665f9e571c3f
HatCherry <kpospiech.tg@gmail.com>
2023-09-01 21:58:56 +0200

    Finished curve_fitting.py with adding exponential and logarithmic curve fit choice

--- curve_fitting.py
+++ curve_fitting.py
@@ -1,50 +1,102 @@
 from scipy.optimize import curve_fit
 import numpy as np
 
-def curve(x, *coefficients):
-    return np.polyval(coefficients, x)
-
-def curve_fitting(x, y, degree):
+def curve_fitting(x_list, y_list):
     """
-    Fits a polynomial curve to given x and y data points and returns the polynomial equation.
+    Performs curve fitting on given data points using specified curve types.
 
     Parameters:
-    - x_list (list of array-like): A list of array-like objects representing the x-coordinates for each set of data points.
-    - y_list (list of array-like): A list of array-like objects representing the y-coordinates for each set of data points.
-    - degrees (list of int): A list of integers specifying the degree of the polynomial to fit for each set of data points.
-
+    - x_list: list of 1D NumPy arrays
+        The x-coordinates of the data points for each data set.
+    - y_list: list of 1D NumPy arrays
+        The y-coordinates of the data points for each data set.
+    
+    User Input:
+    - The type of curve (polynomial, exponential, logarithmic) to fit for each data set.
+    - For polynomial fits, the user also specifies the degree of the polynomial.
+    
     Returns:
-    - list of tuples: Each tuple contains the following:
-        1. array-like: Coefficients of the fitted polynomial for each set of data points.
-        2. str: String representation of the polynomial equation in the form "ax^n + bx^(n-1) + ... + k".
-
-    Example usage:
-    x1 = np.linspace(0, 10, 20)
-    y1 = 2 * x1**2 + 1 + np.random.normal(0, 1, len(x1))
-    x2 = np.linspace(0, 10, 20)
-    y2 = 3 * x2**3 + 1 + np.random.normal(0, 1, len(x2))
-
-    curve_fitting([x1, x2], [y1, y2], [2, 3])
+    - A list of curve coefficients for each data set.
+    
+    Output:
+    - Prints the curve equation in a format that can be used in other Python scripts.
+    
+    Examples:
+    >>> x1 = np.linspace(0, 10, 20)
+    >>> y1 = 2 * x1**2 + 1 + np.random.normal(0, 1, len(x1))
+    >>> curve_fitting([x1], [y1])
+    Select the fit function (polynomial/exponential/logarithmic): polynomial
+    Define the polynomial degree: 2
+    0.9894x**2 + 0.0248*x + 1.0529
     """
-    params, _ = curve_fit(curve, x, y, p0=[1]*(degree+1))
 
-    terms = []
-    for i, coef in enumerate(params):
-        if i == len(params) - 1: # Last term, constant
-            terms.append(f"{coef:.4f}")
-        elif i == len(params) - 2: # Penultimate term, with x
-            terms.append(f"{coef:.4f}*x")
+    curve_coefficients = []
+
+    for x, y in zip(x_list, y_list):
+
+        curve_type = input("Select the fit function (polynomial/exponential/logarithmic): ")
+
+        if curve_type == "polynomial":
+                
+            def curve(x, *coefficients):
+                return np.polyval(coefficients, x)
+            
+            degrees = int(input("Define the polynomial degree: "))  # An example way to define degrees
+
+            p0 = [1] * (degrees + 1)  # Initial guess
+        
+        elif curve_type == "exponential":
+
+            def curve(x, a, b):
+                return a * np.exp(b * x)
+            
+            p0 = [1,1] # Initial guess
+
+        elif curve_type == "logarithmic":
+
+            def curve(x, a, b):
+                return a + b * np.log(x)
+            
+            p0 = [1,1] # Initial guess
+
         else:
-            terms.append(f"{coef:.4f}x**{len(params) - 1 - i}")
+            raise ValueError("Invalid curve type")
+        
+        params, _ = curve_fit(curve, x, y, p0=p0)
+        curve_coefficients.append(params)
 
-    polynomial = " + ".join(terms)
-    polynomial = polynomial.replace(" + -", " - ")
+        if curve_type == "polynomial":
+            terms = []
+            for i, coef in enumerate(params):
+                if i == len(params) - 1: # Last term, constant
+                    terms.append(f"{coef:.4f}")
+                elif i == len(params) - 2: # Penultimate term, with x
+                    terms.append(f"{coef:.4f}*x")
+                else:
+                    terms.append(f"{coef:.4f}x**{len(params) - 1 - i}")
 
-    return polynomial
+            polynomial = " + ".join(terms)
+            polynomial = polynomial.replace(" + -", " - ")
+            print(polynomial)
+
+        elif curve_type == "exponential":
+            exponent = f"{params[0]:.4f} x**{params[1]:.4f}"
+            print(exponent)
+
+        elif curve_type == "logarithmic":
+            logarithm = f"{params[1]:.4f} * log(x) + {params[0]:.4f}"
+            logarithm = logarithm.replace(" + -", " - ")
+            print(logarithm)
+
+    return curve_coefficients
+
 
 # Example usage
-x1 = np.linspace(0, 10, 20)
-y1 = 2 * x1**2 + 1 + np.random.normal(0, 1, len(x1))
+x1 = np.linspace(1, 10, 10)
+y1 = 3 * x1 ** 2 + 2
+x2 = np.linspace(1, 10, 10)
+y2 = 3 * np.exp(0.3 * x2)
+x3 = np.linspace(1, 10, 10)
+y3 = 2 + 1.5 * np.log(x3)
 
-polynomial = curve_fitting(x1, y1, 2)
-print(f"f(x) = {polynomial}")
+a = curve_fitting([x1], [y1])
